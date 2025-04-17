6a3f8ae5bfe8a2612f7df19e6e5ecfa2f415e4c0
Rsoniie <roshansoniee@gmail.com>
2025-04-17 11:47:43 +0530

    Question Api's Done

--- server/routes/questionsRoutes.js
+++ server/routes/questionsRoutes.js
@@ -1,3 +1,12 @@
-import Router from 'express';
+import { Router } from "express";
+import { Pollen, Rhinitis, Mite, Insect, Mold } from "../controllers/questionsControllers.js";
 const router = Router();
 
+router.get('/pollen', Pollen);
+router.get('/rhinitis', Rhinitis);
+router.get('/mite', Mite);
+router.get('/insect', Insect);
+router.get('/mold', Mold);
+
+
+export default router;
