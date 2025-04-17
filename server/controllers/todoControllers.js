ffcbd5fdc42124b11a8a4e7235d850aab46fa68b
Rsoniie <roshansoniee@gmail.com>
2025-04-17 16:29:59 +0530

    Save todo api done

--- server/controllers/todoControllers.js
+++ server/controllers/todoControllers.js
@@ -1,23 +1,38 @@
 import User from "../models/userModel.js";
 
 const saveTodo = async (req, res) => {
-    try {
+  try {
+    const { todo_list } = req.body;
 
-        
-        return res.status(200).json({message : "Successfully in saveTodo"});
-    } catch (error) {
-        return res.status(500).json({message: "Internal server error"});
+    if (!todo_list || !Array.isArray(todo_list)) {
+      return res.status(400).json({ message: "Invalid todo_list format" });
     }
+
+    const userId = req.user.id;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // user.todos = user.todos ? [...user.todos, ...todo_list] : todo_list;
+    user.todos = todo_list.length > 0 ? todo_list : user.todo;
+
+    await user.save();
+
+    return res.status(200).json({ message: "Successfully in saveTodo" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 const getAll = async (req, res) => {
-    try {
-        return res.status(200).json({message : "Successfully getting All"});
-    } catch (error) {
-        return res.status(500).json({message: "Internal server error"});
-    }
-}
+  try {
+    return res.status(200).json({ message: "Successfully getting All" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-
-export {saveTodo, getAll};
-
+export { saveTodo, getAll };
